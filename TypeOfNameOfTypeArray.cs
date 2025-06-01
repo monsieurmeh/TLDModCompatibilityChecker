7a986c818c3235ab954722ea0caab728bf0d2ac8
Nick <101865296+monsieurmeh@users.noreply.github.com>
2025-05-31 21:31:14 -0700

    Delete TypeOfNameOfTypeArray.cs

--- TypeOfNameOfTypeArray.cs
+++
@@ -1,6 +0,0 @@
-using HarmonyLib;
-
-[HarmonyPatch(typeof(TLDModCompatibilityChecker), nameof(TLDModCompatibilityChecker.TypeOfNameOfArray), new Type[] { typeof(int), typeof(string) })]
-public class PatchTypeNameofArgs
-{
-}
